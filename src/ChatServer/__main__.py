147d36547a494674d6cd107557820d386d2a1199
Patrick Thomas <patrick@patrickwthomas.net>
2023-04-02 15:16:21 +0000

    Fix get bot, improve CLI

--- src/ChatServer/__main__.py
+++ src/ChatServer/__main__.py
@@ -1,12 +1,27 @@
+import logging
 from ChatServer.ml import _get_bot
+from datetime import datetime
 
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
+    logging.info("Loading model...")
+    before = datetime.now()
     bot = _get_bot()
+    after = datetime.now()
+    logging.info(f"Done loading model in {after - before}. Enter nothing to quit.")
 
     while True:
         user_input = input(">>> ")
 
         if not user_input:
+            logging.info("Received empty input. Quitting...")
             break
-        print(bot.tell(user_input))
+
+        before = datetime.now()
+        response = bot.tell(user_input)
+        after = datetime.now()
+
+        logging.info(f"Took {after - before} to respond.")
+
+        print(response)
--- src/ChatServer/ml.py
+++ src/ChatServer/ml.py
@@ -4,6 +4,7 @@ from ChatServer.Backend.facebook_blenderbot_3b import FacebookBlenderbot3B
 def _get_bot():
     bot = FacebookBlenderbot3B()
     bot.load()
+    return bot
 
 
 if __name__ == "__main__":
